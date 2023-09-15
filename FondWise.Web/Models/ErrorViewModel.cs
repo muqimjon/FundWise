f6cbcbd9478d09325e6b50d8bcce2270dc5bff0f
Muqimjon <muqimjon.mr@gmail.com>
2023-09-15 22:58:19 +0500

    Cleaned files

--- FondWise.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FondWise.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
